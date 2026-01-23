6a86b0e1447aab04f67a20c6eb0367abe467de18
DoMinhHung <dominhhung04032003@gmail.com>
2026-01-23 15:15:54 +0700

    Project Init

---
+++ src/main/java/iuh/fit/se/tramcamxuc/TramcamxucV2Application.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.tramcamxuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramcamxucV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TramcamxucV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/fit/se/tramcamxuc/TramcamxucV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.tramcamxuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramcamxucV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
