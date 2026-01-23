2a4be9c30fc2cc1d68ff5a4fb2d86d7bc75ae1bd
DoMinhHung <dominhhung04032003@gmail.com>
2026-01-23 15:33:43 +0700

    Add JWT dependencies and enable JPA auditing

--- src/main/java/iuh/fit/se/tramcamxuc/TramcamxucV2Application.java
+++ src/main/java/iuh/fit/se/tramcamxuc/TramcamxucV2Application.java
@@ -2,8 +2,10 @@ package iuh.fit.se.tramcamxuc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TramcamxucV2Application {
 
     public static void main(String[] args) {
